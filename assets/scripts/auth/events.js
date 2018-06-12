a2a30eb625c64395fc7f3e5657c1c902044cf9a0
bentoks2016 <nwogbobaret@gmail.com>
2018-06-12 09:32:42 -0400

    Game now switches between player X & O

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,15 +1,52 @@
 'use strict'
+//
+// const playAsX = function () {
+//   console.log('i am playing as X')
+//   return ('You are player X')
+// }
+//
+// const playAsO = function () {
+//   console.log('i am playing as O')
+//   return ('You are player O')
+// }
+//
+// const onClicking = function () {
+//   event.preventDefault()
+//   if (playAsO) {
+//     console.log('event is', event)
+//     $('#' + event.target.id).html('O')
+//   } else if (playAsX) {
+//     console.log('event is', event)
+//     $('#' + event.target.id).html('X')
+//   } else {
+//     console.log('please select a player')
+//     return ('Please select a player')
+//   }
+// }
 
-const onClicking = function (event) {
-  event.preventDefault()
-  alert(event.target.id)
-  console.log('event is', event)
-  $('#' + event.target.id).html('X')
+const board = []
+let currentPlayer = 'X'
+
+const switchPlayer = function (event) {
+  if (event.target.innerHTML === '') {
+    if (currentPlayer === 'X') {
+      board[event.target.id] = 'X'
+      event.target.innerHTML = 'X'
+
+      currentPlayer = 'O'
+    } else {
+      board[event.target.id] = 'O'
+      event.target.innerHTML = 'O'
+
+      currentPlayer = 'X'
+    }
+  }
 }
-  // document.getElementById('s4').click() {
-  //   console.log('this is different')
-  // }
 
 module.exports = {
-  onClicking: onClicking
+  // onClicking: onClicking,
+  // playAsX: playAsX,
+  // playAsO: playAsO,
+  currentPlayer: currentPlayer,
+  switchPlayer: switchPlayer
 }
